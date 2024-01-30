8c5fa72b7e083b309835cb1470882da626228521
Pmaneemay <mayratdaa.pmanee@gmail.com>
2024-01-30 23:35:21 +0800

    complete user winning bids and luckydraws content

--- frontend/src/components/Events/Events.jsx
+++ frontend/src/components/Events/Events.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import styles from '../../styles/styles'
+import React from "react";
+import styles from "../../styles/styles";
 import EventCard from "./EventCard";
+import { useSelector } from "react-redux";
 
 const Events = () => {
+  const { allBiddings, isLoading } = useSelector((state) => state.biddings);
   return (
-    <div >
-      <div className={`${styles.section}`}>
-        <div className={`${styles.heading}`}>
-          <h1 className='text-teal-600'>Ongoing Bidding</h1>
-        </div>
+    <div>
+      {!isLoading && (
+        <div className={`${styles.section}`}>
+          <div className={`${styles.heading}`}>
+            <h1 className="text-teal-600">Ongoing Bidding</h1>
+          </div>
 
-        <div className="w-full grid">
-          <EventCard />
+          <div className="w-full grid">
+            {allBiddings.length !== 0 && (
+              <EventCard data={allBiddings && allBiddings[0]} />
+            )}
+            <h4>{allBiddings?.length === 0 && "No ongoing bidding!"}</h4>
+          </div>
         </div>
-
-      </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Events
+export default Events;
